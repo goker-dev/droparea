1acc565871c7839338704a7a3a6e603bb3542c35
goker cebeci <dev@gokercebeci.com>
2011-05-12 22:57:33 +0300

    It's more flexible now

--- droparea.js
+++ droparea.js
@@ -1,119 +1,124 @@
 (function( $ ){
-	var area, s;
-	// Methods
-	var m = {
-		traverse: function(files, folderDest, encrypt){
-			if (typeof files !== "undefined") {
-				for (var i=0, l=files.length; i<l; i++) {
-					m.upload(files[i], folderDest, encrypt);
-				}
-			} else {
-				$(area).html(nosupport);
-			}	
-		},
-		upload: function(file, folderDest, encrypt){
-			$(area).empty();
-			var progress = $('<div>',{'class':'progress'}),	xhr, requests;
-			$(area).append(progress);
+    var s;
+    // Methods
+    var m = {
+        init: function(e){},
+        start: function(e){},
+        complete: function(r){},
+        error: function(r){ alert(r.error); return false; },
+        traverse: function(files,area){
+            if (typeof files !== "undefined") {
+                for (var i=0, l=files.length; i<l; i++) {
+                    m.upload(files[i], area);
+                }
+            } else {
+                area.html(nosupport);
+            }
+        },
+        upload: function(file, area){
+            //area.empty();
+            var progress = $('<div>',{
+                'class':'progress'
+            });
+            area.append(progress);
 			
-			// File type control
-			if (typeof FileReader === "undefined" || !(/image/i).test(file.type)) {
-				$(area).html(file.type,s.noimage); 
-				alert('only image files: jpeg, png, gif');
-				return false;
-			}
+            // File type control
+            if (typeof FileReader === "undefined" || !(/image/i).test(file.type)) {
+                //area.html(file.type,s.noimage);
+                alert('only image files: jpeg, png, gif');
+                return false;
+            }
 
-			// File size control
-			if (file.size > (s.maxsize * 1024)) {
-				$(area).html(file.type,s.noimage); 
-				alert('max upload size: ' + s.maxsize + 'Kb');
-				return false;
-			}
+            // File size control
+            if (file.size > (s.maxsize * 1024)) {
+                //area.html(file.type,s.maxsize);
+                alert('max upload size: ' + s.maxsize + 'Kb');
+                return false;
+            }
 			
-			// Uploading - for Firefox, Google Chrome and Safari
-			xhr = new XMLHttpRequest();
-			// Update progress bar
-			xhr.upload.addEventListener("progress", function (e) {
-				if (e.lengthComputable) {
-					var loaded = Math.ceil((e.loaded / e.total) * 100) + "%";
-					progress.css({'height':loaded}).html(loaded);
-				}
-			}, false);
+            // Uploading - for Firefox, Google Chrome and Safari
+            var xhr = new XMLHttpRequest();
+            // Update progress bar
+            xhr.upload.addEventListener("progress", function (e) {
+                if (e.lengthComputable) {
+                    var loaded = Math.ceil((e.loaded / e.total) * 100) + "%";
+                    progress.css({
+                        'height':loaded
+                    }).html(loaded);
+                }
+            }, false);
 			
-			// File uploaded
-			xhr.addEventListener("load", function (e) {
-			    $(area).html(e.target.responseText);
-				progress.addClass('uploaded');
-				progress.html(s.uploaded).fadeOut('slow');
-			}, false);
+            // File uploaded
+            xhr.addEventListener("load", function (e) {
+                var r = jQuery.parseJSON(e.target.responseText);
+                s.complete(r);
+                area.find('img').remove();
+                area.data('value',r.filename)
+                .append($('<img>',{'src': r.path + r.filename + '?' + Math.random()}));
+                progress.addClass('uploaded');
+                progress.html(s.uploaded).fadeOut('slow');
+            }, false);
 			
-			// Set request parameters
-			requests = '?width=' + $(area).data('width')
-    			     + '&height=' + $(area).data('height')
-    			     + '&type=' + $(area).data('type')
-    			     + '&crop=' + $(area).data('crop')
-    			     + '&quality=' + $(area).data('quality');
-			
-			xhr.open("post", s.post + requests, true); 
+            xhr.open("post", s.post, true);
             
             // Set appropriate headers
-		    xhr.setRequestHeader("Content-Type", "multipart/form-data");
-		    xhr.setRequestHeader("X-File-Name", file.fileName);
-		    xhr.setRequestHeader("X-File-Size", file.fileSize);
-		    xhr.setRequestHeader("X-File-Type", file.type);
-			xhr.setRequestHeader("X-File-Location", folderDest + '/');
-			xhr.setRequestHeader("X-File-Encrypt", encrypt);
+            xhr.setRequestHeader("content-type", "multipart/form-data");
+            xhr.setRequestHeader("x-file-name", file.fileName);
+            xhr.setRequestHeader("x-file-size", file.fileSize);
+            xhr.setRequestHeader("x-file-type", file.type);
 
-		    xhr.send(file);
-		}
-	};
-	/**
-	* @param folderDest string specify the destination folder
-	* @param encrypt string active encryption
-	*/
-	$.fn.droparea = function(o) {
-		// Settings
-	    s = {
-			'instructions'	: 'drop an image file here',
-			'over'			: 'drop file here!',
-			'nosupport'		: 'No support for the File API in this web browser',
-			'noimage'		: 'Unsupported file type!',
-			'uploaded'		: 'Uploaded',
-			'maxsize'		: '500', //Kb
-			'post'			: 'upload.php',
-			'folderDest'    : '',
-			'encrypt'		: 'yes'
-	    };
-		this.each(function(){
-			if(o) $.extend(s, o);
-		    $(this)
-			    .html(s.instructions)
-			    .bind({
-			    dragleave: function (e) {
-					e.preventDefault();
-					e.stopPropagation();
-					var target = e.target;
-					if (target && target === area) {
-						$(this).removeClass('over').html(s.instructions);
-					}
-				},
-				dragenter: function (e) {
-					e.preventDefault();
-					e.stopPropagation();
-					area = this;
-			    	$(this).addClass('over').html(s.over);
-				},
-				dragover: function (e) {
-					e.preventDefault();
-					e.stopPropagation();
-				}
-			    });
-			this.addEventListener("drop", function (e) {
-				m.traverse(e.dataTransfer.files, s.folderDest, s.encrypt);
-				e.preventDefault();
-		    	e.stopPropagation();
-				$(this).removeClass('over');
-			},false);				
-		});
-	};
+            // Set request headers
+            for (var i in area.data())
+                if (typeof area.data(i) !== "object")
+                    xhr.setRequestHeader('x-param-'+i, area.data(i));
+
+            xhr.send(file);
+        }
+    };
+    $.fn.droparea = function(o) {
+        // Settings
+        s = {
+            'init': m.init,
+            'start': m.start,
+            'complete': m.complete,
+            'instructions': 'drop an image file here',
+            'over'        : 'drop file here!',
+            'nosupport'   : 'No support for the File API in this web browser',
+            'noimage'     : 'Unsupported file type!',
+            'uploaded'    : 'Uploaded',
+            'maxsize'     : '500', //Kb
+            'post'        : 'upload.php'
+        };
+        this.each(function(){
+            if(o) $.extend(s, o);
+            var instructions = $('<div>').appendTo($(this));
+            s.init($(this));            
+            if(!$(this).data('value'))
+                instructions.addClass('instructions').html(s.instructions);
+
+            $(this)
+            .bind({
+                dragleave: function (e) {
+                    e.preventDefault();
+                    if($(this).data('value'))
+                        instructions.removeClass().empty();
+                    else
+                        instructions.removeClass('over').html(s.instructions);
+                },
+                dragenter: function (e) {
+                    e.preventDefault();
+                    instructions.addClass('instructions over').html(s.over);
+                },
+                dragover: function (e) {
+                    e.preventDefault();
+                }
+            });
+            this.addEventListener("drop", function (e) {
+                e.preventDefault();
+                s.start($(this));
+                m.traverse(e.dataTransfer.files, $(this));
+                instructions.removeClass().empty();
+            },false);
+        });
+    };
 })( jQuery );
--- upload.php
+++ upload.php
@@ -3,61 +3,70 @@
 // Maximum file size
 $maxsize = 1024; //Kb
 // Supporting image file types
-$types = Array('image/png','images/gif','image/jpeg');
+$types = Array('image/png', 'images/gif', 'image/jpeg');
 
 $headers = getallheaders();
 
 // LOG
-$log = '=== '. @date('Y-m-d H:i:s') . ' ========================================'."\n"
-		.'HEADER:'.print_r($headers,1)."\n"
-		.'GET:'.print_r($_GET,1)."\n"
-		.'POST:'.print_r($_POST,1)."\n"
-		.'REQUEST:'.print_r($_REQUEST,1)."\n"
-		.'FILES:'.print_r($_FILES,1)."\n";
-$fp = fopen('log.txt','a');
+$log = '=== ' . @date('Y-m-d H:i:s') . ' ===============================' . "\n"
+        . 'HEADERS:' . print_r($headers, 1) . "\n";
+$fp = fopen('log.txt', 'a');
 fwrite($fp, $log);
 fclose($fp);
 
-header('content-type: plain/text');
+// Result object
+$r = new stdClass();
+// Result content type
+header('content-type: application/json');
 
 // File size control
-if($headers['X-File-Size'] > ($maxsize *1024)) {
-    die("Max file size: $maxsize Kb");
+if ($headers['x-file-size'] > ($maxsize * 1024)) {
+    $r->error = "Max file size: $maxsize Kb";
 }
 
+$folder = $headers['x-param-folder'] ? $headers['x-param-folder'] . '/' : '';
+if ($folder && !is_dir($folder))
+    mkdir($folder);
+
 // File type control
-if(in_array($headers['X-File-Type'],$types)){
+if (in_array($headers['x-file-type'], $types)) {
     // Create an unique file name    
-	if($headers['X-File-Encrypt'] == 'true' || $headers['X-File-Encrypt'] == 'TRUE' ) {
-		$filename = $headers['X-File-Location'].sha1(@date('U').'-'.$headers['X-File-Name']).'.'.$_GET['type'];
-	}else {
-		$filename = $headers['X-File-Location'].$headers['X-File-Name'];
-	}
+    if ($headers['x-param-value']) {
+        $filename = $folder . $headers['x-param-value'];
+    } else {
+        $filename = $folder . sha1(@date('U') . '-' . $headers['x-file-name'])
+                . '.' . $headers['x-param-type'];
+    }
     // Uploaded file source
     $source = file_get_contents('php://input');
-	//Check if file exists in destination folder (Fix for encrypt = false)
-	if(file_exists($path.$filename)) {
-		$filename = $headers['X-File-Location'].$headers['X-File-Name'].'_'.@date('his').'.'.$_GET['type'];
-	}
     // Image resize
-    imageresize($source, $filename, $_GET['width'], $_GET['height'], $_GET['crop'], $_GET['quality']);
-} else die("Unsupported file type: ".$headers['X-File-Type']);
+    imageresize($source, $filename,
+            $headers['x-param-width'],
+            $headers['x-param-height'],
+            $headers['x-param-crop'],
+            $headers['x-param-quality']);
+} else
+    $r->error = "Unsupported file type: " . $headers['x-file-type'];
 
 // File path
-$path = str_replace('upload.php','',$_SERVER['SCRIPT_NAME']);
+$path = str_replace('upload.php', '', $_SERVER['SCRIPT_NAME']);
 // Image tag
-echo '<img src="'.$path.$filename.'" alt="image" />';
+$r->filename = $filename;
+$r->path = $path;
+$r->img = '<img src="' . $path . $filename . '" alt="image" />';
+echo json_encode($r);
 
 // Image resize function with php + gd2 lib
 function imageresize($source, $destination, $width = 0, $height = 0, $crop = false, $quality = 80) {
+    $quality = $quality ? $quality : 80;
     $image = imagecreatefromstring($source);
     if ($image) {
         // Get dimensions
         $w = imagesx($image);
         $h = imagesy($image);
-        if (($width && $w > $width) || ($height && $height > $h)) {
+        if (($width && $w > $width) || ($height && $h > $height)) {
             $ratio = $w / $h;
-            if (($ratio >= 1 || $height == 0) && !$crop) {
+            if (($ratio >= 1 || $height == 0) && $width && !$crop) {
                 $new_height = $width / $ratio;
                 $new_width = $width;
             } elseif ($crop && $ratio <= ($width / $height)) {
@@ -74,6 +83,7 @@ function imageresize($source, $destination, $width = 0, $height = 0, $crop = fal
         $x_mid = $new_width * .5;  //horizontal middle
         $y_mid = $new_height * .5; //vertical middle
         // Resample
+        error_log('height: '.$new_height.' - width: '.$new_width);
         $new = imagecreatetruecolor(round($new_width), round($new_height));
         imagecopyresampled($new, $image, 0, 0, 0, 0, $new_width, $new_height, $w, $h);
         // Crop
@@ -85,7 +95,7 @@ function imageresize($source, $destination, $width = 0, $height = 0, $crop = fal
         // Output
         // Enable interlancing [for progressive JPEG]
         imageinterlace($crop ? $crop : $new, true);
-        
+
         $dext = strtolower(pathinfo($destination, PATHINFO_EXTENSION));
         if ($dext == '') {
             $dext = $ext;
